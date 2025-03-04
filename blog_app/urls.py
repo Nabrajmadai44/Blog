caeb10bafbf23761f0b834a665cc89fd90c597ca
nabrajmadai44 <nabraj0444@gmail>
2025-03-04 19:33:49 +0545

    blog

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,4 +3,8 @@ from blog_app import views
 
 urlpatterns = [
     path("", views.post_list, name="post-list"),
+    path("post_detail/<int:pk>/", views.post_detail, name="post-detail"),
+    path("post_delete/<int:pk>/", views.post_delete, name= "post-delete"),
+    path("draft_list", views.draft_list, name= "draft-list"),
+    path("post_edit/<int:pk>/", views.post_edit, name = "post-edit"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog_app.models import Post
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def post_list(request):
@@ -8,4 +9,39 @@ def post_list(request):
         request,
         "post_list.html",
         {"posts": posts},
+    )
+    
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=False)
+    return render(
+        request,
+        "post_detail.html",
+        {"post":post}
+    )
+
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect("post-list")
+
+
+@login_required
+def draft_list(request):
+    posts = Post.objects.filter(published_at__isnull=True)
+    return render(
+        request,
+        "draft_list.html",
+        {"posts": posts},
+    )
+
+
+
+def post_edit(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.edit()
+    return render(
+        request,
+        'post_edit.html',
+        { 'post': post }
     )
